0716f8ffaaab1cd85d59235e9f45d9ce4d68b4bf
Fred Wu <ifredwu@gmail.com>
2021-09-20 17:38:39 +1000

    Added docker support

---
+++ Dockerfile
@@ -0,0 +1,21 @@
+FROM elixir:1.12-alpine
+
+WORKDIR /ex_double_entry
+
+RUN set -ex; \
+    apk add --no-cache \
+        build-base \
+        git \
+        mariadb-dev \
+        postgresql-dev \
+        sqlite-dev \
+        ;
+
+RUN mix local.hex --force && \
+    mix local.rebar --force
+
+COPY . ./
+
+ENV MIX_ENV=test
+
+RUN mix do deps.get, deps.compile
--- config/test.exs
+++ config/test.exs
@@ -3,8 +3,8 @@ import Config
 config :ex_double_entry, ExDoubleEntry.Repo,
   username: "postgres",
   password: "postgres",
-  database: "ex_double_entry_test",
-  hostname: "localhost",
+  database: System.get_env("POSTGRES_DB", "ex_double_entry_test"),
+  hostname: System.get_env("POSTGRES_HOST", "localhost"),
   pool: Ecto.Adapters.SQL.Sandbox,
   show_sensitive_data_on_connection_error: true
 
