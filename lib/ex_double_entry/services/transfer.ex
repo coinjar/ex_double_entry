52e8474cbf6433fffb5c71b510e899907f9aa41b
Fred Wu <ifredwu@gmail.com>
2021-09-22 11:45:30 +1000

    Elixirfy `ensure_accounts`

--- lib/ex_double_entry/services/transfer.ex
+++ lib/ex_double_entry/services/transfer.ex
@@ -68,19 +68,19 @@ defmodule ExDoubleEntry.Transfer do
     end)
   end
 
-  defp ensure_accounts_if_needed(ensure?, acc_a, acc_b) do
-    if ensure? == true do
-      {
-        acc_a |> AccountBalance.for_account!() |> Account.present(),
-        acc_b |> AccountBalance.for_account!() |> Account.present(),
-      }
-    else
-      cond do
-        is_nil(AccountBalance.for_account(acc_a)) ->
-          raise Account.NotFoundError
-        is_nil(AccountBalance.for_account(acc_b)) ->
-          raise Account.NotFoundError
-      end
+  defp ensure_accounts_if_needed(true, acc_a, acc_b) do
+    {
+      acc_a |> AccountBalance.for_account!() |> Account.present(),
+      acc_b |> AccountBalance.for_account!() |> Account.present(),
+    }
+  end
+
+  defp ensure_accounts_if_needed(_, acc_a, acc_b) do
+    cond do
+      is_nil(AccountBalance.for_account(acc_a)) ->
+        raise Account.NotFoundError
+      is_nil(AccountBalance.for_account(acc_b)) ->
+        raise Account.NotFoundError
     end
   end
 end
